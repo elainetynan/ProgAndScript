eb7eb3394dd6a089bd93d4f395fa8cc59bc2cc20
Elaine.Tynan <G00411232@gmit.ie>
2022-03-09 19:45:27 +0000

    Fixed question 13

--- Week08/Lab08/question13.py
+++ Week08/Lab08/question13.py
@@ -22,8 +22,8 @@ with open(filename, "rt") as f:
             legends.append(allFields[0])
             legends.append(allFields[1])
         else:
-            temperatureList.append(allFields[0])
-            salesList.append(allFields[1])
+            temperatureList.append(int(allFields[0]))
+            salesList.append(int(allFields[1]))
         count += 1
 
 print(legends)
@@ -31,20 +31,7 @@ print(legends)
 temperatures = np.array(temperatureList)
 sales = np.array(salesList)
 
-# Try to set axes before plotting
-# https://subscription.packtpub.com/book/big_data_/9781849513265/3/ch03lvl1sec48/controlling-tick-spacing
-#ax  = plt.axes()
-#ax.xaxis.set_major_locator(ticker.MultipleLocator(500))
-#ax.xaxis.set_minor_locator(ticker.MultipleLocator(250))
-
 plt.plot(salesList, temperatureList)
-
-#specify x-axis labels
-# https://www.statology.org/set-x-axis-values-matplotlib/
-#xTicks = [0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000, 7500, 8000, 8500, 9000, 9500] 
-#add x-axis values to plot
-#plt.xticks(ticks=xTicks)
-
 
 plt.title("Beverage Sales")
 plt.xlabel("Sales")
